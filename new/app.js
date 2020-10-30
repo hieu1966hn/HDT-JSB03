849610dd62f84535951b5f63dac659b794089960
NGUYEN TRUNG HIEU <hieu1966hn@gmail.com>
2020-10-30 19:24:38 +0700

    code session 5 day nhe

---
+++ new/app.js
@@ -0,0 +1,34 @@
+// // khai bao bien: let, var, const;
+// // const a = 5;
+// // let b = 10;
+// // let c = a / b;
+
+
+// // kieu dl: undefine,String, Number, Boolean, Array, Object,....
+
+
+// // console.log("day la gia tri a = " + a);
+// // document.write("day la gia tri a = " + a);
+// // document.write(a+b+j);
+
+
+
+// // tinh dien tich tam giac ( biet a,b,c)
+// let a, b, c, p, S;
+// a = Number(prompt("nhap vao a:"));
+// b = Number(prompt("nhap vao b"));
+// c = Number(prompt("nhap vao c"));
+
+// p = (a + b + c) / 2;
+
+// if (a + b > c && b + c > a && c + a > b) {
+//     S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+//     console.log("hi")
+//     console.log("vay dien tich tam giac la: " + S)
+// }
+// else {
+//     alert("tam giac ban nhap khong ton tai");
+// }
+
+console.log("hello \n guy");
+// // console.log("hello");
